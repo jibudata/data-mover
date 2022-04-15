792da9995088cfe486160c82f2e2164b93df92c2
heping <heping@jibudata.com>
2022-04-15 23:51:44 +0800

    #412: udpate in queue timeout

--- controllers/veleroexport_controller.go
+++ controllers/veleroexport_controller.go
@@ -50,7 +50,7 @@ const (
 	requeueAfterFast = 5 * time.Second
 	requeueAfterSlow = 20 * time.Second
 	FailureTimeout   = 15 * time.Minute
-	QueuedTimeout    = 30 * time.Minute
+	QueuedTimeout    = 255 * time.Minute
 )
 
 const (
